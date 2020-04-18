d715ffec6edbcadc4d6b16f9395737a8aa9a9898
h42m0ny <harmonyleclaire@gmail.com>
2020-04-18 14:11:29 +0200

    replace checkbox by radiobutton

--- app/src/main/java/com/fellowship/attestationgenerator/MainActivity.kt
+++ app/src/main/java/com/fellowship/attestationgenerator/MainActivity.kt
@@ -8,12 +8,10 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.CompoundButton
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.RadioButton
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var editTextFirstName: EditText? = null;
@@ -28,13 +26,13 @@ class MainActivity : AppCompatActivity() {
 
     private var buttonSubmit: Button? = null;
 
-    private var cbTravail: CheckBox? = null;
-    private  var cbCourses:CheckBox? = null;
-    private  var cbSante:CheckBox? = null;
-    private  var cbFamille:CheckBox? = null;
-    private  var cbSport:CheckBox? = null;
-    private  var cbJudiciaire:CheckBox? =null;
-    private  var cbMissions:CheckBox? = null;
+    private var cbTravail: RadioButton? = null;
+    private  var cbCourses:RadioButton? = null;
+    private  var cbSante:RadioButton? = null;
+    private  var cbFamille:RadioButton? = null;
+    private  var cbSport:RadioButton? = null;
+    private  var cbJudiciaire:RadioButton? =null;
+    private  var cbMissions:RadioButton? = null;
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,36 +50,43 @@ class MainActivity : AppCompatActivity() {
         editTextTime = findViewById<View>(R.id.time) as EditText;
         initDateAndTime(editTextDateSortie!!, editTextTime!!);
 
-        cbTravail = findViewById<View>(R.id.chk_travail) as CheckBox;
-        cbCourses = findViewById<View>(R.id.chk_courses) as CheckBox;
-        cbSante = findViewById<View>(R.id.chk_sante) as CheckBox;
-        cbFamille = findViewById<View>(R.id.chk_famille) as CheckBox;
-        cbSport = findViewById<View>(R.id.chk_sport) as CheckBox;
-        cbJudiciaire = findViewById<View>(R.id.chk_judiciaire) as CheckBox;
-        cbMissions = findViewById<View>(R.id.chk_missions) as CheckBox;
+        cbTravail = findViewById<View>(R.id.chk_travail) as RadioButton;
+        cbCourses = findViewById<View>(R.id.chk_courses) as RadioButton;
+        cbSante = findViewById<View>(R.id.chk_courses) as RadioButton;
+        cbFamille = findViewById<View>(R.id.chk_famille) as RadioButton;
+        cbSport = findViewById<View>(R.id.chk_sport) as RadioButton;
+        cbJudiciaire = findViewById<View>(R.id.chk_judiciaire) as RadioButton;
+        cbMissions = findViewById<View>(R.id.chk_missions) as RadioButton;
 
 
-        cbTravail!!.setOnClickListener(View.OnClickListener {
-            onCheckboxClicked(it);
-        });
-        cbCourses!!.setOnClickListener(View.OnClickListener {
-            onCheckboxClicked(it);
-        });
-        cbSante!!.setOnClickListener(View.OnClickListener {
-            onCheckboxClicked(it);
-        });
-        cbFamille!!.setOnClickListener(View.OnClickListener {
-            onCheckboxClicked(it);
-        });
-        cbSport!!.setOnClickListener(View.OnClickListener {
-            onCheckboxClicked(it);
-        });
-        cbJudiciaire!!.setOnClickListener(View.OnClickListener {
-            onCheckboxClicked(it);
-        });
-        cbMissions!!.setOnClickListener(View.OnClickListener {
-           onCheckboxClicked(it);
-        });
+//        cbTravail!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//            onCheckboxClicked(it);
+//        });
+//        cbCourses!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//            onCheckboxClicked(it);
+//        });
+//        cbSante!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//            onCheckboxClicked(it);
+//        });
+//        cbFamille!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//            onCheckboxClicked(it);
+//        });
+//        cbSport!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//            onCheckboxClicked(it);
+//        });
+//        cbJudiciaire!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//            onCheckboxClicked(it);
+//        });
+//        cbMissions!!.setOnClickListener(View.OnClickListener {
+//            it as RadioButton;
+//           onCheckboxClicked(it);
+//        });
 
 
 
@@ -118,14 +123,10 @@ class MainActivity : AppCompatActivity() {
         };
     }
 
-    private fun onCheckboxClicked(view : View) {
-        if (view is CheckBox ) {
-            if(!view.isChecked){
-                view.isChecked = true;
-            }
-        }
-
-    }
+//    private fun onCheckboxClicked(cb : RadioButton) {
+//
+//
+//    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initDateAndTime(editTxtDatesortie : EditText, editTxtTime : EditText) {
